fa1907c4689dffdf806f5b3c78270c17b5a05cd7
victor-singha <vicunite2@gmail.com>
2021-07-17 18:49:42 +0530

    [victor] Book Item CSS

--- Bookstore/Controllers/StoreController.cs
+++ Bookstore/Controllers/StoreController.cs
@@ -12,7 +12,7 @@ namespace Bookstore.Controllers
             BookModel book = new BookModel();
             List<BookModel> books = new List<BookModel>
             {
-                new BookModel {BookId = 1,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.5",Reviews = 22,OriginalPrice = 2000,DiscountPrice = 1200},
+                new BookModel {BookId = 1,Name = "UX Design",Author = "Cabbage",Image = "https://static-cse.canva.com/blob/142538/Purple-and-Red-Leaves-Illustration-Childrens-Book-Cover.jpg",Rating = "4.5",Reviews = 22,OriginalPrice = 2000,DiscountPrice = 1200},
                 new BookModel {BookId = 2,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.1",Reviews = 62,OriginalPrice = 2200,DiscountPrice = 1000},
                 new BookModel {BookId = 3,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.3",Reviews = 42,OriginalPrice = 4000,DiscountPrice = 1300},
                 new BookModel {BookId = 4,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.8",Reviews = 29,OriginalPrice = 8000,DiscountPrice = 1700},
