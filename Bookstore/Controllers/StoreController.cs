cd5bce1f320b9d463885af15d993480ccc43a56e
victor-singha <vicunite2@gmail.com>
2021-07-17 17:36:02 +0530

    [victor] BookModel

--- Bookstore/Controllers/StoreController.cs
+++ Bookstore/Controllers/StoreController.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using CommonLayer;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Bookstore.Controllers
@@ -11,6 +9,16 @@ namespace Bookstore.Controllers
         // GET: Store
         public ActionResult Books()
         {
+            BookModel book = new BookModel();
+            List<BookModel> books = new List<BookModel>
+            {
+                new BookModel {BookId = 1,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.5",Reviews = 22,OriginalPrice = 2000,DiscountPrice = 1200},
+                new BookModel {BookId = 2,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.1",Reviews = 62,OriginalPrice = 2200,DiscountPrice = 1000},
+                new BookModel {BookId = 3,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.3",Reviews = 42,OriginalPrice = 4000,DiscountPrice = 1300},
+                new BookModel {BookId = 4,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "4.8",Reviews = 29,OriginalPrice = 8000,DiscountPrice = 1700},
+                new BookModel {BookId = 5,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "3.5",Reviews = 35,OriginalPrice = 5400,DiscountPrice = 2700},
+                new BookModel {BookId = 6,Name = "UX Design",Author = "Cabbage",Image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEcbvZUQNVbxQJv9SeZVQ79MOd6ucMSUeMUg&usqp=CAU",Rating = "5.0",Reviews = 23,OriginalPrice = 2010,DiscountPrice = 1000}
+            };
             return View();
         }
     }
---
+++ CommonLayer/BookModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLayer
+{
+    public class BookModel
+    {
+        public int BookId { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Image { get; set; }
+        public string Rating { get; set; }
+        public int Reviews { get; set; }
+        public int OriginalPrice { get; set; }
+        public int DiscountPrice { get; set; }
+    }
+}
